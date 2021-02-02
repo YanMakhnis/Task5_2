f2ba37cbd49ea83375c24b538331b40b013f135c
Yan <makhnis@gmail.com>
2021-02-02 22:15:21 +0300

    first

---
+++ src/academy/belhard/entity/Person.java
@@ -0,0 +1,10 @@
+package academy.belhard.entity;
+
+public class Person {
+    protected String firstName;
+    protected String lastName;
+
+    public Person(String firstName,String lastName){
+
+    }
+}
